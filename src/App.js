bdab8c6412aedc97bc0e2c2687d698d9ce071c39
maxim e <efr1tt@yandex.ru>
2023-01-15 23:20:49 +0300

    add react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Header from './components/Header'
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Cart } from './pages/Cart'
 
 import './scss/app.scss'
 
 
 function App() {
 
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('https://63bebf9b585bedcb36b5c7c6.mockapi.io/items')
-      .then(res => res.json())
-      .then(arr => {
-        setItems(arr)
-        setIsLoading(false)
-      })
-  }, [])
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />) : items.map((item, index) => (
-                <PizzaBlock {...item} key={item.id} />
-              ))
-            }
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </div>
       </div>
     </div>
